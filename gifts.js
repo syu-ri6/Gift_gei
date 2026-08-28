33e3f2b8247736cee1ecc666e178097b911af46f
12dnc14yuk-blip <12dnc14yuk@gmail.com>
2026-08-28 17:49:44 +0900

    Add files via upload

---
+++ gifts.js
@@ -0,0 +1,317 @@
+window.GIFT_DATA = [
+  {
+    "title": "お歌弾幕",
+    "category": "弾幕",
+    "shuri": "",
+    "response": "🎼.•*¨*•.♪💪🏻🐰💗🎵•*¨*•.¸¸💪🏻🐰💥🎶 .•*¨*•.♪🎵💪🏻🐰💗•*¨*•.¸¸🎶✨👊🏻✨",
+    "locked": true,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "がんばれ弾幕",
+    "category": "弾幕",
+    "shuri": "せーのがせーで",
+    "response": "がんばれ‼️頑張れ‼️がんばれ‼️頑張れ‼️がんばれ‼️頑張れ‼️がんばれ‼️頑張れ‼️がんばれ‼️頑張れ‼️がんばれ‼️頑張れ‼️がんばれ‼️頑張れ‼️だ‼️",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "枠終わり",
+    "category": "定番",
+    "shuri": "次も回復しましょ🎶あっぷっぷ❗️",
+    "response": "あっぷっぷ",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "クラッカー",
+    "category": "ギフト",
+    "shuri": "しゅりの魅力にクラクラクラッカーってこと？そゆこと？助かっちゃったな🎉",
+    "response": "そゆことー\nor\n-` ̗ -` ̗ 🎉ﾅｲｸﾗ 🎉 ̖ ´- ̖ ´-",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "またくるよ",
+    "category": "ギフト",
+    "shuri": "またくるよくれたっことは、お約束❗️",
+    "response": "やくそくー",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "ちこぉよれ",
+    "category": "掛け合い",
+    "shuri": "〜〜〜〜ちこぉよれ〜❗️",
+    "response": "おるよー",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "ガチャチケ",
+    "category": "ギフト",
+    "shuri": "幸せなら拍手しよ🎶",
+    "response": "👏👏",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "くまさん・うさぎさん（大）（小）",
+    "category": "ギフト",
+    "shuri": "",
+    "response": "チーム友達‼️",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "愛している。",
+    "category": "ギフト",
+    "shuri": "",
+    "response": "愛してるっ🫶\nちゅっ‼️\nﾊﾞｲ👋✨ﾊﾞｲ🤙✨ﾊﾞｲ🤟✨ﾊﾞｲ🙌✨",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "赤面チーク",
+    "category": "ギフト",
+    "shuri": "ぽっ",
+    "response": "ぽっ",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "自己紹介",
+    "category": "定番",
+    "shuri": "",
+    "response": "チェケチェケ(σﾟ∀ﾟ)σ☝︎( ^ = ^)☝︎ｱｹﾞｱｹﾞ\n🐰う ぇ る か む ‼️",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "ガチ恋口上",
+    "category": "口上",
+    "shuri": "",
+    "response": "言いたいことがあるんだよ🫵🏻❤️‍🔥\nやっぱりしゅりりはかわいいよ😆💘\n好き好き大好きやっぱすき😘🫶🏼\nやっと見つけたお姫様👸✨️\n俺が生まれて来た理由👶🏻🍼︎\nそれはしゅりりに出会うため👨🚹👩🚺\n俺と一緒に人生歩もう🚶‍♀️💭\n世界で1番愛してる💕\n❤️あ❤️い❤️し❤️て❤️る❤️",
+    "locked": true,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "コール❗️(ポケモンmix)",
+    "category": "コール",
+    "shuri": "",
+    "response": "パン🍞パン🍞パン🍞パン🍞ポケモンパン◓⃙⁣ 👾\nフレッシュ💎🍃ブレッド🥐伊藤パン🥖\n松たか子👩🏻松たか子👩🏻🎀\n山崎🏔春の🌸パン🍞祭り🏮👘✨",
+    "locked": true,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "やれんのか",
+    "category": "掛け合い",
+    "shuri": "やれんのかっていけんのかって",
+    "response": "ﾎｲ!ﾎﾎｲ!",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "💥🔫",
+    "category": "掛け合い",
+    "shuri": "ばーん！",
+    "response": "うわあぁぁぁぁあああああああぁぁあ",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "love so sweet",
+    "category": "曲",
+    "shuri": "",
+    "response": "ジャジャジャジャーン!!",
+    "locked": true,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "一触即発禅ガール",
+    "category": "曲",
+    "shuri": "",
+    "response": "┏( ^o^)┓ﾄﾞｺﾄﾞｺﾄﾞｺﾄﾞｺ┗( ^o^)┛ﾄﾞｺﾄﾞｺﾄﾞｺﾄﾞｺ",
+    "locked": true,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "SAN値",
+    "category": "曲",
+    "shuri": "",
+    "response": "＼(・ω・＼)SAN値!(／・ω・)／ピンチ!",
+    "locked": true,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "決め立ち",
+    "category": "定番",
+    "shuri": "",
+    "response": "-` ̗ -` ̗ 💃ｷﾒﾀﾞﾁ🕺 ̖ ´- ̖ ´-",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "えいちぴー↑↑",
+    "category": "定番",
+    "shuri": "",
+    "response": "-` ̗ -` ̗ 💪🏻ﾅｲｴﾋﾟ🐰 ̖ ´- ̖ ´-\n-` ̗ -` ̗ 💪🏻ﾅ ｲ ｴ ﾋﾟ🐰 ̖ ´- ̖ ´-",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "24時になった時",
+    "category": "時間",
+    "shuri": "",
+    "response": "バトンタッチ❗️",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "食べ物ギフト",
+    "category": "ギフト",
+    "shuri": "",
+    "response": "いっぱい食べるきみがすき〜🎶",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "ちがうとき",
+    "category": "定番",
+    "shuri": "",
+    "response": "いっぱい食べるきみがすき〜🎶",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "火炎放射",
+    "category": "ギフト",
+    "shuri": "",
+    "response": "🔥 A CHI CHI A CHI 🔥",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "あざらひ",
+    "category": "ギフト",
+    "shuri": "",
+    "response": "あざらしのざらしあまざらし🦭",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "いるか",
+    "category": "ギフト",
+    "shuri": "",
+    "response": "いーるーか！いかいか！\nるるるるるるるる〜",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "はいかわいい・今日も可愛い・いつも可愛い",
+    "category": "定番",
+    "shuri": "",
+    "response": "きゅるん",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "高嶺の花子さん",
+    "category": "曲",
+    "shuri": "好きなアイスの味はきっと〜",
+    "response": "みんなの好きな味を教えてね！",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "名称未設定",
+    "category": "掛け合い",
+    "shuri": "しぬ",
+    "response": "そせい",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "おみくじスロット(3個)が揃わなかった時",
+    "category": "ギフト",
+    "shuri": "",
+    "response": "ｻﾞﾝﾈｰﾝ",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "流れ金魚",
+    "category": "ギフト",
+    "shuri": "",
+    "response": "ｷﾝｷﾞｮﾄﾞﾝ",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "何が言いたいの？って時",
+    "category": "定番",
+    "shuri": "",
+    "response": "覚悟？",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "いぇいぇ",
+    "category": "定番",
+    "shuri": "",
+    "response": "うぉうぉ",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "えぐい",
+    "category": "定番",
+    "shuri": "",
+    "response": "えっぐえっぐ🥚",
+    "locked": false,
+    "note": "",
+    "action": ""
+  },
+  {
+    "title": "でか草",
+    "category": "定番",
+    "shuri": "",
+    "response": "╲/╲/ ╲/╲/ ╲/╲/ ╲/╲/ ╲/╲/",
+    "locked": false,
+    "note": "",
+    "action": ""
+  }
+];
